14d4d65e070f8055cf7f0b21a68f02d4cfe94aee
romai <romainlerme@hotmail.fr>
2022-11-04 19:51:01 +0100

    bon ...

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const axios = require('axios'); 
-const ejsLayouts = require('express-ejs-layouts');
+const ejsLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb+srv://root:root@cluster0.mjewup0.mongodb.net/?retryWrites=true&w=majority').then(res => console.log('connecté')).catch(err => console.log(err));
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
@@ -11,7 +14,7 @@ app.use(ejsLayouts);
 
 // GET / - main index of site
 app.get('/', (req, res) => {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151&offset=0';
   // Use request to call the API
   axios.get(pokemonUrl).then(apiResponse => {
     let pokemon = apiResponse.data.results;
@@ -19,8 +22,10 @@ app.get('/', (req, res) => {
   })
 });
 
+
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
+app.use('/test', require('./routes/test'));
 
 app.listen(port, () => {
   console.log('...listening on', port );
